0dcfcd26dc403ec195dbccffc96c58bdb1f3119d
Michael Schmitz <michael@schmitztech.com>
2019-02-27 11:55:57 -0800

    Add skiff files.

---
+++ .skiff/deploy.sh
@@ -0,0 +1,42 @@
+#
+# This script exists to perform variable substitution on a Kubernetes configuration
+# before sending it to the kubectl command.
+#
+
+#!/bin/bash
+set -e
+
+# Get the path to this script.
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+usage() {
+  echo ""
+  echo "Usage:"
+  echo "  ./deploy.sh DOMAIN"
+  echo ""
+  echo "  DOMAIN  the desired domain, i.e. 'buoy.dev.apps.allenai.org'"
+  echo ""
+}
+
+domain=$1
+if [[ -z "$domain" ]]; then
+  echo "Error: no domain specified."
+  usage
+  exit 1
+fi
+
+# Disable failing on errors since Kubernetes has a bug and dies when a namespace doesn't exist.
+# See TODO(michaels).
+set +e
+kubectl get namespace/buoy &> /dev/null
+namespace_exists=$?
+set -e
+if [[ "$namespace_exists" != "0" ]]; then
+  echo "creating buoy namespace..."
+  kubectl create namespace buoy
+else
+  echo "namespace buoy already exists..."
+fi;
+
+# Substitute variables with information passed in and forward the configuration to kubectl.
+sed "s#%DOMAIN%#$domain#g" < $dir/kube.yaml | kubectl apply -f -
